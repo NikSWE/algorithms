d40c25db317f08e79bb03860b4e3cbf57468a21e
muj-programmer <nick.muj.programmer@gmail.com>
2019-11-21 20:12:29 +0530

    :zap: improve performance

--- sorting_algorithms/bubble_sort.c
+++ sorting_algorithms/bubble_sort.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 void print_array(int arr[], int n);
 void bubble_sort(int arr[], int n);
 
 int main()
 {
-    int arr[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+    int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     printf("Unsorted Array: ");
@@ -36,6 +37,8 @@ void bubble_sort(int arr[], int n)
     for (int i = 0; i < n - 1; i++)
     {
         int temp;
+        bool is_sorted = true;
+
         for (int j = 0; j < n - i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
@@ -43,10 +46,14 @@ void bubble_sort(int arr[], int n)
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
+                is_sorted = false;
             }
         }
 
         printf("Pass %d: ", i + 1);
         print_array(arr, n);
+
+        if (is_sorted)
+            break;
     }
 }
